573c48ef5d389303842fbafb1d6fae2b6d87fee2
“xueyuhao” <www.bzd.com>
2017-03-25 18:15:01 +0800

    revise js finder

--- youzan/flex/js/index.js
+++ youzan/flex/js/index.js
@@ -1,19 +1,25 @@
 //实现页面图片切换功能
-  var colors=["#ea3838","#1089c7","#ff8f04","#ffbe00","#19c971","#1069cf","#ff1548","#471f7a"],
-      isSwitched=0,
+  //var colors=["#ea3838","#1089c7","#ff8f04","#ffbe00","#19c971","#1069cf","#ff1548","#471f7a"],
+  var isSwitched=0,
       ban_info=document.getElementsByClassName("ban_info"),
-      ban_a=document.getElementsByClassName("ban_a")
+      ban_a=document.getElementsByClassName("ban_a"),
       change_active=document.getElementById("change_active"),
-      banners==document.getElementById("banners"),
-      n=0,
+      banners=document.getElementById("banners"),
+      n=0;
+      //第一次加载配置页面
       for (var i=0;i<ban_a.length;i++){
         //console.dir(ban_a[0].children[0]);
-        ban_a[i].children[0].style.color=colors[i];
+        //ban_a[i].children[0].style.color=colors[i];
+        //查看第二个ban_a的class没有内容隐藏
         ban_a[i].children[1].innerHTML==""&&(ban_a[i].children[1].style.display="none");
         ban_info[i].children[0].style.backgroundImage = "url(./img/index_banner_"+(i+1)+"@2x.jpg)";
-
       }
-      change_active.onmousemove=change_active.touch=change_active.onclick=function(e){
+    
+    //绑定鼠标事件
+    change_active.onmousemove=change_active.touch=change_active.onclick=mouseEvent;
+
+    //鼠标进入事件方法
+    function mouseEvent(e){
       if (isSwitched==0){
       //是否在切换状态 -->
         var interval=null;
@@ -34,8 +40,8 @@
             }
             <!-- 修改背景样式等属性 -->
             target.className="active";
-            ban_info[n].style.backgroundColor = colors[n];
-            banners.style.backgroundColor = colors[n];
+            //ban_info[n].style.backgroundColor = colors[n];
+            //banners.style.backgroundColor = colors[n];
             ban_info[n].style.display="block";
             ban_info[n].style.opacity="1";
         }
@@ -43,3 +49,5 @@
         return;
       }
     }
+    //鼠标进入事件方法结束
+
